366450e2e562e3dffe39a500b7442f7b6c02e283
IhorWenher <73756717+IhorWenher@users.noreply.github.com>
2021-11-12 22:49:59 +0200

    add poster and default image

--- src/components/MoviesListByTitle/MoviesListByTitle.js
+++ src/components/MoviesListByTitle/MoviesListByTitle.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Style from './MoviesListByTitle.module.css';
+import defaultImage from '../../images/defaultPhoto.jpeg';
 
 const MoviesListByTitle = ({ moviesBySearch }) => {
   return (
-    <ul>
+    <ul className={Style.list}>
       {moviesBySearch &&
-        moviesBySearch.map(({ id, title }) => (
+        moviesBySearch.map(({ id, title, original_name, original_title, poster_path }) => (
           <li className={Style.li} key={id}>
-            <Link to={`/movies/${id}`}>{title}</Link>
+            <Link to={`/movies/${id}`}><img className={Style.img}
+              src={poster_path 
+                ? `https://image.tmdb.org/t/p/w300${poster_path}` 
+                : defaultImage
+                }
+              alt={original_title ? original_title : original_name}
+            />
+            {original_title ? (
+              <p className={Style.p}>{original_title}</p>
+            ) : (
+              <p className={Style.p}>{original_name}</p>
+            )}</Link>
           </li>
         ))}
     </ul>
--- src/components/MoviesListTrending/MoviesListTrending.js
+++ src/components/MoviesListTrending/MoviesListTrending.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Style from './MoviesListTrending.module.css';
+import defaultImage from '../../images/defaultPhoto.jpeg';
 
 const MoviesListTrending = ({ movies }) => {
   return (
@@ -9,7 +10,18 @@ const MoviesListTrending = ({ movies }) => {
       {movies.map(({ original_title, original_name, id, poster_path }) => (
         <li key={id} className={Style.li}>
           <Link to={`/movies/${id}`}>
-            {original_title ? original_title : original_name}
+            <img className={Style.img}
+              src={poster_path 
+                ? `https://image.tmdb.org/t/p/w300${poster_path}` 
+                : defaultImage
+                }
+              alt={original_title ? original_title : original_name}
+            />
+            {original_title ? (
+              <p className={Style.p}>{original_title}</p>
+            ) : (
+              <p className={Style.p}>{original_name}</p>
+            )}
           </Link>
         </li>
       ))}
